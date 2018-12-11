66aa28bc5acaf3f46251076728a608c7c390e45b
PeterWinzell <peterwinzell.gbg@gmail.com>
2018-12-11 10:48:55 -0800

    initial Go commit

---
+++ w3cGo/w3c.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/reply", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for sake of simplicity
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "Websockets.html")
+		fmt.Printf("websockets.html template served/n")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
