f244e9dcee0af5a0296dedec29845909da4d5ccd
PeterWinzell <peterwinzell.gbg@gmail.com>
2019-03-04 09:47:55 -0800

    added comments

--- server/Go/server-1.0/logger.go
+++ server/Go/server-1.0/logger.go
@@ -1,1 +1,35 @@
 package main
+
+/************************************************************************************
+
+Logrus has seven logging levels: Trace, Debug, Info, Warning, Error, Fatal and Panic.
+Ex:
+ log.Error("read: ", err)
+ ...
+ log.WithFields(log.Fields{
+		"Method": r.Method,
+		"Host":r.Host,
+		"Proto":r.Proto,
+		"Uri":r.RequestURI,
+	}).Info("http request fields")
+...
+For more info : https://github.com/sirupsen/logrus/blob/master/README.md
+
+***********************************************************************************/
+
+import (
+	"os"
+	log "github.com/sirupsen/logrus"
+)
+
+
+func initLogger(){
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stdout instead of the default stderr
+	// Can be any io.Writer, see below for File example
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.TraceLevel)
+}
