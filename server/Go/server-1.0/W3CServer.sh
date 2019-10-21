fb3a5e36b18b524a53ec2e701ce744864affad5f
PeterWinzell <peterwinzell.gbg@gmail.com>
2019-10-21 15:30:39 -0700

    verified server functionality , removed logs

--- server/Go/server-1.0/W3CServer.sh
+++ server/Go/server-1.0/W3CServer.sh
@@ -3,6 +3,7 @@
 
 
 startme() {
+    echo "starting ..."
     screen -d -m -S serverCore bash -c 'cd server-core  && go build && ./server-core'
     screen -d -m -S serviceMgr bash -c 'go run service_mgr.go'
     screen -d -m -S wsMgr bash -c 'go run ws_mgr.go'
