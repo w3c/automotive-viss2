dad3c8c2c295205a19fb3391e6c8851eb4b9e7cb
magnus.gunnarsson <magnus.gunnarsson@melcogot.com>
2021-09-16 19:38:48 +0200

    added argparse to mqtt_client and brokertest since

--- server/signal_broker/brokertest.go
+++ server/signal_broker/brokertest.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/MEAE-GOT/W3C_VehicleSignalInterfaceImpl/utils"
+	"github.com/akamensky/argparse"
 	log "github.com/sirupsen/logrus"
 	//	"github.com/MEAE-GOT/W3C_VehicleSignalInterfaceImpl/server/Go/server-1.0/server-core/signal_broker"
 )
 
 func main() {
 
-	utils.InitLog("brokerlog","")
+	parser := argparse.NewParser("print", "Prints provided string to stdout")
+	// Create string flag
+	logFile := parser.Flag("", "logfile", &argparse.Options{Required: false, Help: "outputs to logfile in ./logs folder"})
+	logLevel := parser.Selector("", "loglevel", []string{"trace", "debug", "info", "warn", "error", "fatal", "panic"}, &argparse.Options{
+		Required: false,
+		Help:     "changes log output level",
+		Default:  "info"})
+	// Parse input
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+	}
+
+	utils.InitLog("brokerlog", "", *logFile, *logLevel)
 	// Test connection
 	conn, response := GetResponseReceiver()
 	defer conn.Close()
